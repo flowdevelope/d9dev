197779b3bee27eedda553126a87d540f29be8cbf
Olaf Urban <o.urban@flowconcept.de>
2020-10-18 18:51:21 +0200

    Modify theme

--- ground/vanity/js/slider.js
+++ ground/vanity/js/slider.js
@@ -2,6 +2,35 @@
   Drupal.behaviors.Slider = {
     attach: function (context, settings) {
 
+
+      $('.field-galery').unwrap();
+      $('.field-galery').unwrap();
+      $('.field-galery').unwrap();
+      $('.imagegalery', context).once().slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        dots: true,
+        arrows: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>",
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ]
+      });
+
     }
   }
 })(jQuery, Drupal);
--- ground/vanity/js/vanity.js
+++ ground/vanity/js/vanity.js
@@ -18,7 +18,9 @@
         slidesToScroll: 1,
         autoplay: false,
         dots: true,
-        arrows: false,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>",
       });
 
     }
