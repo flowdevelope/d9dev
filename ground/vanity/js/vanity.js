4de858388c5111e8a65c6f553b27397eeea50c75
Olaf Urban <o.urban@flowconcept.de>
2020-10-18 08:50:00 +0200

    Ground zero theme for all projects in future

--- ground/vanity/js/vanity.js
+++ ground/vanity/js/vanity.js
@@ -2,6 +2,25 @@
   Drupal.behaviors.Vanity = {
     attach: function (context, settings) {
 
+      $('.topnav a', context).on('click touch', function(e){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+          scrollTop: $(target).offset().top - 10
+        }, 2000);
+      });
+
+      $('.field-team').unwrap();
+      $('.field-team').unwrap();
+      $('.field-team').unwrap();
+      $('.team-slider', context).once().slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        dots: true,
+        arrows: false,
+      });
+
     }
   }
 
