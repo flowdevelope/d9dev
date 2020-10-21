6dfb36a977adea6e64665f50a922eb46dcc1ec02
Olaf Urban <o.urban@flowconcept.de>
2020-10-21 06:43:48 +0200

    Modufy the ultimate theme

--- ground/vanity/js/slider.js
+++ ground/vanity/js/slider.js
@@ -31,6 +31,20 @@
         ]
       });
 
+      $('.field-team').unwrap();
+      $('.field-team').unwrap();
+      $('.field-team').unwrap();
+      $('.team-slider', context).once().slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        dots: true,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>",
+      });
+
     }
   }
 })(jQuery, Drupal);
--- ground/vanity/js/vanity.js
+++ ground/vanity/js/vanity.js
@@ -2,25 +2,30 @@
   Drupal.behaviors.Vanity = {
     attach: function (context, settings) {
 
-      $('.topnav a', context).on('click touch', function(e){
-        var target = $(this).attr('href');
-        $('html, body').animate({
-          scrollTop: $(target).offset().top - 10
-        }, 2000);
+      $(window).on('load resize', function() {
+        var win = $(this);
+        $('.topnav a', context).on('click touch', function (e) {
+          var target = $(this).attr('href');
+          $('html, body').animate({
+            scrollTop: $(target).offset().top - 10
+          }, 2000);
+          if (win.width() < 1023) {
+            $('.navbar-collapse').removeClass('show');
+            $('.overlay').toggle('slow');
+            $('header').removeClass('headercolor');
+          }
+        });
       });
 
-      $('.field-team').unwrap();
-      $('.field-team').unwrap();
-      $('.field-team').unwrap();
-      $('.team-slider', context).once().slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        dots: true,
-        arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>",
+      $('.navbar-toggle', context).on('click touch', function(e){
+        $('.overlay').toggle('slow');
+        $('header').toggleClass('headercolor');
+      });
+
+      $('.rolldown', context).on('click touch', function(e){
+        $('html, body').animate({
+          scrollTop: $('#kontakt').offset().top - 10
+        }, 2000);
       });
 
     }
